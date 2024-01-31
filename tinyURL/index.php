076ff479e82b6509b77c060a2f604664abf544f7
seba0456 <46867564+seba0456@users.noreply.github.com>
2024-01-31 18:23:59 +0100

    added css

--- tinyURL/index.php
+++ tinyURL/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shorten your URL!</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -12,17 +13,19 @@
     <h2>Shorten your URL!</h2>
 
     <form action="process.php" method="post">
-        <label for="UserUrl">Full Url:</label>
+
+        <label for="UserUrl">Paste yours long Url:</label><br>
         <input type="url" id="UserUrl" name="UserUrl" required><br>
-        <label for="vol">Expires:</label>
+        <label for="vol">Select expire date:</label><br>
         <input type="range" id="vol" name="vol" min="0" max="5" value="0" step="1" onchange="updateRangeValue()">
+        <br><input type="submit" value="Shorten URL!">
+        <!-- add Captcha someday...-->
         <div class="expires">
-            <p>Expires <span id="rangeValue">Next day</span>.</p>
+            <p>Yours link expires <span id="rangeValue">Next day</span>.</p>
         </div>
 
-<input type="submit" value="Submit">
-<!-- add Captcha -->
     </form>
+
 
     <script>
         function updateRangeValue() {
@@ -55,6 +58,17 @@
             rangeValueSpan.innerHTML = rangeToStr;
         }
     </script>
+    <hr>
+    <footer>
+        <h3>Authors:</h3>
+        <div class="links">
+            <a href="https://github.com/seba0456">
+                <img src="images/github-mark.png" alt="Github profile" style="width:42px;height:42px;">
+                <br><b>seba0456</b>
+            </a>
+        </div>
+
+    </footer>
 </body>
 
 </html>
